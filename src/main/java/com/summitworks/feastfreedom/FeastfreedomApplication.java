e788a852efa281a11c0ba85ce90ccc0521b761ee
Tom Benjamin <tomjben7@gmail.com>
2020-06-03 12:17:23 -0400

    Initial commit

---
+++ src/main/java/com/summitworks/feastfreedom/FeastfreedomApplication.java
@@ -0,0 +1,13 @@
+package com.summitworks.feastfreedom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeastfreedomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeastfreedomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/summitworks/feastfreedom/FeastfreedomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summitworks.feastfreedom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeastfreedomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
